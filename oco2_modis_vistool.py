ba80dc5959d9117ca32efc316b2f9df19c446ab6
Heather Cronk <heather.cronk@colostate.edu>
2016-08-19 10:19:09 -0600

    Initial commit. Updates GIBS XML and pulls GIBS RGB

---
+++ oco2_modis_vistool.py
@@ -0,0 +1,87 @@
+"""
+oco2_modis_vistool.py
+
+The purpose of this script is to pull Aqua-MODIS RGB images from Worldview
+using the NASA GIBS API and overlay various OCO-2 data fields for case study 
+analysis in support of OCO-2 cloud and aerosol screening validation.
+
+GIBS developer documentation:https://wiki.earthdata.nasa.gov/display/GIBS/GIBS+API+for+Developers
+
+Minimum command line call:
+python oco2_modis_vistool.py
+
+Requirements:
+
+1) json configuration file (default: oco2_modis_vistool_config.json in code directory)
+2) This script is meant to run on ocomaster at Colorado State University. 
+   To run it elsewhere, the data locations will need to be updated 
+   and modules will need to be verified.
+
+Authors:
+Natalie Tourville <natalie.tourville@colostate.edu>
+Heather Cronk <heather.cronk@colostate.edu>
+
+Revision history:
+08/2016 (N.D. Tourville; H.Q. Cronk): Initial version
+
+"""
+
+import os
+import sys
+
+from OCO2FileOps import *
+
+import numpy as np
+
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from osgeo import gdal, osr
+from shapely.ops import transform as geom_transform
+
+import xml.etree.ElementTree as ET
+
+import re
+
+def update_GIBS_xml(date, xml_file):
+
+    """
+    Puts the date of interest into the GIBS XML file
+    """
+    
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    url = root[0][0].text
+
+    old_date = re.split('/', url)[6]
+
+    new_url = re.sub(old_date, date, url)
+
+    root[0][0].text = new_url
+    tree.write(xml_file)
+    
+def pull_Aqua_RGB_GIBS(lat_ul, lon_ul, lat_lr, lon_lr, xml_file, tif_file, xsize=1200, ysize=1000):
+    
+    """
+    Pulls the Aqua RGB imagery from WorldView using GIBS and puts it in specified tif file with associated metadata
+    """ 
+    
+    cmd = "gdal_translate -of GTiff -outsize "+str(xsize)+" "+str(ysize)+" -projwin "+str(lon_ul)+" "+str(lat_ul)+" "+str(lon_lr)+" "+str(lat_lr)+" "+xml_file+" "+tif_file
+
+    os.system(cmd)
+
+date = '2015-07-04'
+code_dir = os.path.dirname(os.path.realpath(__file__))
+xml_file = code_dir+'/GIBS_Aqua_MODIS_truecolor.xml'
+
+update_GIBS_xml(date, xml_file)
+#pull_Aqua_RGB_GIBS(43.5, -78.9, 39.6, -77.5,  xml_file, 'testing_python_pull.tif')
+#pull_Aqua_RGB_GIBS(5, -79, -18, -48,  xml_file, 'testing_python_pull.tif')
+ 
+
+
+
+
