0ded6bbb5d55e58257e35c51913f29a4d6402947
Aronne Merrelli <aronne.merrelli@ssec.wisc.edu>
2022-06-13 16:30:57 +0000

    add a message to the console for the case where the background_image is skipped, when overlay data is not present. due to recent changes, the code could simply return without doing anything, because of the new logic that skips all imagery when the overlay data is missing.

--- oco_vistool.py
+++ oco_vistool.py
@@ -1529,7 +1529,10 @@ if __name__ == "__main__":
         # image regardless of the input setting; we have no way to know the
         # desired background time in this case.
         else:
-            make_background_image = False
+            if ovr_d['make_background_image']:
+                print('No overlay data present in the target box after applying filters. '+
+                      'stopping background image creation')
+                make_background_image = False
     else:
         # otherwise, if NO overlay dictionary was specified in the input config,
         # then this is automatically a background-only run.
